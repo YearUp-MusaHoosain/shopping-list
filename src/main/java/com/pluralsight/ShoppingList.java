f3bfd6db69052e890a781e8a5153bfb13e3b89c3
Musa Hoosain <musahoosain@gmail.com>
2024-09-25 15:13:33 -0400

    completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args)
+    {
+        System.out.println("Shopping list:");
+        System.out.println("------------------------");
+        System.out.println("Apple");
+        System.out.println("Oranges");
+        System.out.println("Chicken");
+        System.out.println("Chicken");
+
+    }
+}
